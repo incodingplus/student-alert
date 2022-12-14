d0a7f623e97aa89b8cf959e78a1d88618dfe9794
myrlagksruf <myrlagksruf@gmail.com>
2022-12-14 15:25:35 +0900

    지시사항에 선생님 추가

--- src/modalinput.ts
+++ src/modalinput.ts
@@ -24,19 +24,24 @@ export const studentJisi = async (inter:Interaction<CacheType>, channel:string, 
     if (!command.has(inter.commandName)) return false;
     if (inter.channelId !== channel) return false;
     const reg = /^([가-힣]+)\d{5}$/;
+    const tReg = /^([가-힣]+)\s선생님/;
     const data = command.get(inter.commandName)();
     const modal = new ModalBuilder()
         .setCustomId(inter.commandName)
         .setTitle(data.title);
 
     let member = inter.options.data[0].member as GuildMember;
-
+    let tMember = inter.member as GuildMember;
     const embed = new EmbedBuilder();
     let user = reg.test(member.nickname) ? member.nickname : member.user.username;
-    if(!user){
-        embed
-            .setColor("Red")
-            .setTitle("이 유저는 적절한 학생이 아닙니다.");
+    let teacher = tReg.test(tMember.nickname) ? tMember.nickname : tMember.user.username;
+    if(!teacher || !user){
+        embed.setColor('Red')
+        if(!teacher){
+            embed.setTitle('선생님만 메세지를 보낼 수 있습니다.');
+        } else {
+            embed.setTitle("이 유저는 적절한 학생이 아닙니다.");
+        }
         await inter.reply({ embeds: [embed], ephemeral:true });
         return;
     }
@@ -72,6 +77,7 @@ export const studentJisi = async (inter:Interaction<CacheType>, channel:string, 
     });
     const result:[string, string][] = [
         ['이름', user.match(reg)[1]],
+        ['선생님', teacher.match(tReg)[1]],
         ...data.value.map(v => {
             let val = modalInter.fields.getTextInputValue(v.id);
             if(v.check){
