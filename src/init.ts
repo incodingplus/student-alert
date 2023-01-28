591b92a2a0b04bc0c457800f6d73e44fd44399c6
myrlagksruf <myrlagksruf@gmail.com>
2023-01-28 13:04:00 +0900

    실수로 신규 날린거 수정

--- src/init.ts
+++ src/init.ts
@@ -5,212 +5,236 @@ import {
 } from 'discord.js';
 const commands = [
     {
-        name:'지시',
-        description:'지시 사항을 내릴 학생을 적어주세요.',
-        options:[
+        name: '지시',
+        description: '지시 사항을 내릴 학생을 적어주세요.',
+        options: [
             {
-                name:'이름',
-                description:'학생 이름을 선택해주세요.',
-                type:6,
+                name: '이름',
+                description: '학생 이름을 선택해주세요.',
+                type: 6,
                 required: true,
             }
         ]
-    },
-  {
-    name: '상담',
-    description: '학생 상담 내용 공지',
-    options:[
-        {
-            name:'이름',
-            description:'학생 이름을 작성해주세요.',
-            type:3,
-            required: true,
-        },{
-            name:'일시',
-            description:"'MM-DD' 이런 형식으로 날짜를 작성해주세요.",
-            type:3,
-            required: true,
-        },{
-            name:'내용',
-            description:'상담 내용을 작성해주세요.',
-            type:3,
-            required: true,
-        }
-    ]
-  },{
-    name: '퇴원',
-    description: '퇴원 학생 공지',
-    options:[
-      {
-          name:'이름',
-          description:'학생 이름을 작성해주세요.',
-          type:3,
-          required: true,
-      },{
-          name:'일시',
-          description:"'MM-DD' 이런 형식으로 날짜를 작성해주세요.",
-          type:3,
-          required: true,
-      },{
-          name:'과목',
-          description:'과목명을 작성해주세요.',
-          type:3,
-          required: true,
-      },{
-          name:'사유',
-          description:'퇴원 사유를 작성해주세요.',
-          type:3,
-          required: true,
-      }
-  ]
-  },{
-    name: '변경',
-    description: '수업 변경 공지',
-    options:[
-      {
-          name:'이름',
-          description:'학생 이름을 작성해주세요.',
-          type:3,
-          required: true,
-      },{
-          name:'기존일시',
-          description:"'MM-DD-HH' 또는 'MM-DD-HH:mm' 이런 형식으로 날짜를 작성해주세요.",
-          type:3,
-          required: true,
-      },{
-        name:'변경일시',
-        description:"'MM-DD-HH' 또는 'MM-DD-HH:mm' 이런 형식으로 날짜를 작성해주세요.",
-        type:3,
-        required: true,
-      },{
-          name:'과목',
-          description:'과목명을 작성해주세요.',
-          type:3,
-          required: true,
-      },{
-          name:'사유',
-          description:'수업 변경 사유를 작성해주세요.',
-          required:true,
-          type:3,
-      }
-  ]
-  },{
-    name: '보충',
-    description: '보충 수업 공지',
-    options:[
-      {
-          name:'이름',
-          description:'학생 이름을 작성해주세요.',
-          type:3,
-          required: true,
-      },{
-          name:'일시',
-          description:"'MM-DD-HH' 또는 'MM-DD-HH:mm' 이런 형식으로 날짜를 작성해주세요.",
-          type:3,
-          required: true,
-      },{
-          name:'과목',
-          description:'과목명을 작성해주세요.',
-          type:3,
-          required: true,
-      },{
-          name:'특이사항',
-          description:'특이사항이 있다면 작성해주세요.',
-          type:3,
-      }]
-  },{
-    name: '예약',
-    description: '학생 상담 예약',
-    options:[
-        {
-            name:'이름',
-            description:'학생 이름을 작성해주세요.',
-            type:3,
-            required: true,
-        },{
-            name:'학년',
-            description:'학생의 학년을 작성해주세요.',
-            type:3,
-            required: true,
-        },{
-            name:'일시',
-            description:"'MM-DD-HH' 또는 'MM-DD-HH:mm' 이런 형식으로 날짜를 작성해주세요.",
-            type:3,
-            required: true,
-        },{
-            name:'특이사항',
-            description:'특이사항이 있다면 작성해주세요.',
-            type:3,
-        }
-    ]
-  },{
-    name: '결석',
-    description: '결석 학생 공지',
-    options:[
-      {
-          name:'이름',
-          description:'학생 이름을 작성해주세요.',
-          type:3,
-          required: true,
-      },{
-          name:'일시',
-          description:"'MM-DD-HH' 또는 'MM-DD-HH:mm' 이런 형식으로 날짜를 작성해주세요.",
-          type:3,
-          required: true,
-      },{
-          name:'과목',
-          description:'과목명을 작성해주세요.',
-          type:3,
-          required: true,
-      },{
-          name:'사유',
-          description:'결석 사유를 작성해주세요.',
-          type:3,
-          required: true,
-      }
-  ]
-  },{
-    name: '비대면',
-    description: '비대면 수업 학생 공지',
-    options:[
-      {
-          name:'이름',
-          description:'학생 이름을 작성해주세요.',
-          type:3,
-          required: true,
-      },{
-          name:'일시',
-          description:"'MM-DD-HH' 또는 'MM-DD-HH:mm' 이런 형식으로 날짜를 작성해주세요.",
-          type:3,
-          required: true,
-      },{
-          name:'과목',
-          description:'과목명을 작성해주세요.',
-          type:3,
-          required: true,
-      },{
-          name:'사유',
-          description:'비대면 수업 사유를 작성해주세요.',
-          type:3,
-          required: true,
-      }
-    ]
-  }, {
-    name:'메세지 삭제',
-    type:3,
-  }
+    }, {
+        name: '신규',
+        description: '신규 학생 등록 공지',
+        options: [
+            {
+                name: '이름',
+                description: '학생 이름을 작성해주세요.',
+                type: 3,
+                required: true,
+            }, {
+                name: '일시',
+                description: "'MM-DD-HH' 또는 'MM-DD-HH:mm' 이런 형식으로 날짜를 작성해주세요.",
+                type: 3,
+                required: true,
+            }, {
+                name: '과목',
+                description: '과목명을 작성해주세요.',
+                type: 3,
+                required: true,
+            }, {
+                name: '특이사항',
+                description: '특이사항이 있다면 작성해주세요.',
+                type: 3,
+            }
+        ]
+    }, {
+        name: '상담',
+        description: '학생 상담 내용 공지',
+        options: [
+            {
+                name: '이름',
+                description: '학생 이름을 작성해주세요.',
+                type: 3,
+                required: true,
+            }, {
+                name: '일시',
+                description: "'MM-DD' 이런 형식으로 날짜를 작성해주세요.",
+                type: 3,
+                required: true,
+            }, {
+                name: '내용',
+                description: '상담 내용을 작성해주세요.',
+                type: 3,
+                required: true,
+            }
+        ]
+    }, {
+        name: '퇴원',
+        description: '퇴원 학생 공지',
+        options: [
+            {
+                name: '이름',
+                description: '학생 이름을 작성해주세요.',
+                type: 3,
+                required: true,
+            }, {
+                name: '일시',
+                description: "'MM-DD' 이런 형식으로 날짜를 작성해주세요.",
+                type: 3,
+                required: true,
+            }, {
+                name: '과목',
+                description: '과목명을 작성해주세요.',
+                type: 3,
+                required: true,
+            }, {
+                name: '사유',
+                description: '퇴원 사유를 작성해주세요.',
+                type: 3,
+                required: true,
+            }
+        ]
+    }, {
+        name: '변경',
+        description: '수업 변경 공지',
+        options: [
+            {
+                name: '이름',
+                description: '학생 이름을 작성해주세요.',
+                type: 3,
+                required: true,
+            }, {
+                name: '기존일시',
+                description: "'MM-DD-HH' 또는 'MM-DD-HH:mm' 이런 형식으로 날짜를 작성해주세요.",
+                type: 3,
+                required: true,
+            }, {
+                name: '변경일시',
+                description: "'MM-DD-HH' 또는 'MM-DD-HH:mm' 이런 형식으로 날짜를 작성해주세요.",
+                type: 3,
+                required: true,
+            }, {
+                name: '과목',
+                description: '과목명을 작성해주세요.',
+                type: 3,
+                required: true,
+            }, {
+                name: '사유',
+                description: '수업 변경 사유를 작성해주세요.',
+                required: true,
+                type: 3,
+            }
+        ]
+    }, {
+        name: '보충',
+        description: '보충 수업 공지',
+        options: [
+            {
+                name: '이름',
+                description: '학생 이름을 작성해주세요.',
+                type: 3,
+                required: true,
+            }, {
+                name: '일시',
+                description: "'MM-DD-HH' 또는 'MM-DD-HH:mm' 이런 형식으로 날짜를 작성해주세요.",
+                type: 3,
+                required: true,
+            }, {
+                name: '과목',
+                description: '과목명을 작성해주세요.',
+                type: 3,
+                required: true,
+            }, {
+                name: '특이사항',
+                description: '특이사항이 있다면 작성해주세요.',
+                type: 3,
+            }]
+    }, {
+        name: '예약',
+        description: '학생 상담 예약',
+        options: [
+            {
+                name: '이름',
+                description: '학생 이름을 작성해주세요.',
+                type: 3,
+                required: true,
+            }, {
+                name: '학년',
+                description: '학생의 학년을 작성해주세요.',
+                type: 3,
+                required: true,
+            }, {
+                name: '일시',
+                description: "'MM-DD-HH' 또는 'MM-DD-HH:mm' 이런 형식으로 날짜를 작성해주세요.",
+                type: 3,
+                required: true,
+            }, {
+                name: '특이사항',
+                description: '특이사항이 있다면 작성해주세요.',
+                type: 3,
+            }
+        ]
+    }, {
+        name: '결석',
+        description: '결석 학생 공지',
+        options: [
+            {
+                name: '이름',
+                description: '학생 이름을 작성해주세요.',
+                type: 3,
+                required: true,
+            }, {
+                name: '일시',
+                description: "'MM-DD-HH' 또는 'MM-DD-HH:mm' 이런 형식으로 날짜를 작성해주세요.",
+                type: 3,
+                required: true,
+            }, {
+                name: '과목',
+                description: '과목명을 작성해주세요.',
+                type: 3,
+                required: true,
+            }, {
+                name: '사유',
+                description: '결석 사유를 작성해주세요.',
+                type: 3,
+                required: true,
+            }
+        ]
+    }, {
+        name: '비대면',
+        description: '비대면 수업 학생 공지',
+        options: [
+            {
+                name: '이름',
+                description: '학생 이름을 작성해주세요.',
+                type: 3,
+                required: true,
+            }, {
+                name: '일시',
+                description: "'MM-DD-HH' 또는 'MM-DD-HH:mm' 이런 형식으로 날짜를 작성해주세요.",
+                type: 3,
+                required: true,
+            }, {
+                name: '과목',
+                description: '과목명을 작성해주세요.',
+                type: 3,
+                required: true,
+            }, {
+                name: '사유',
+                description: '비대면 수업 사유를 작성해주세요.',
+                type: 3,
+                required: true,
+            }
+        ]
+    }, {
+        name: '메세지 삭제',
+        type: 3,
+    }
 ];
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
-  try {
-    console.log('Started refreshing application (/) commands.');
+    try {
+        console.log('Started refreshing application (/) commands.');
 
-    let test = await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD), { body: commands });
-    console.log(test)
-    console.log('Successfully reloaded application (/) commands.');
-  } catch (error) {
-    console.error(error);
-  }
+        let test = await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD), { body: commands });
+        console.log(test)
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
 })();
