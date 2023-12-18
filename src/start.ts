9b125f9f6f8225bdede7853ac0c4b1d267314323
myrlagksruf <myrlagksruf@gmail.com>
2023-12-18 16:16:08 +0900

    process.exit 추가

--- src/start.ts
+++ src/start.ts
@@ -1,25 +1,22 @@
 const decode = new TextDecoder()
-const start = async () => {
-    const secret = crypto.randomUUID();
-    const sub = Bun.spawn(['bun', 'run', 'src/index.ts', secret])
-    const reader = sub.stdout.getReader()
-    while(true){
-        const { done, value } = await reader.read()
-        if(done){
-            console.log('프로그램 종료')
-            break
-        }
-        const str = decode.decode(value)
-        console.log(str)
-        if(str.trim().includes(secret)){
-            console.log('프로그램 killed')
-            break
-        }
+const secret = crypto.randomUUID();
+const sub = Bun.spawn(['bun', 'run', 'src/index.ts', secret])
+const reader = sub.stdout.getReader()
+while(true){
+    const { done, value } = await reader.read()
+    if(done){
+        console.log('프로그램 종료')
+        break
     }
-    sub.kill(9)
-    const { stdout } = Bun.spawnSync(['git', 'pull'])
-    console.log(decode.decode(stdout))
-    console.log('git pull 완료')
+    const str = decode.decode(value)
+    console.log(str)
+    if(str.trim().includes(secret)){
+        console.log('프로그램 killed')
+        break
+    }
 }
-
-start()
+sub.kill(9)
+const { stdout } = Bun.spawnSync(['git', 'pull'])
+console.log(decode.decode(stdout))
+console.log('git pull 완료')
+process.exit(0);
